c1b90bbce77e26001a88870f441c7180c2f3037b
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-01-19 14:57:30 +0100

    Prepares PHPUnit

---
+++ tests/FeederTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class FeederTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_test()
+    {
+        $this->assertEquals(true, true);
+    }
+}
