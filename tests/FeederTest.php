319cd277145d791b82d0a1aa7e38d7e44e240f3b
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-01-19 16:29:28 +0100

    Scaffold FeederTest

--- tests/FeederTest.php
+++ tests/FeederTest.php
@@ -1,12 +1,27 @@
 <?php
 
+use GuzzleHttp\Client;
+use Tonik\Feeder\Feed\TwitterFeed;
+use Tonik\Feeder\Feeder;
+
 class FeederTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @test
      */
-    public function it_test()
+    public function it_should_throw_on_missing_twitter_api_keys()
     {
-        $this->assertEquals(true, true);
+        $client = $this->getClient();
+        $feeder = $this->getFeeder($client);
+    }
+
+    public function getClient()
+    {
+        return Mockery::mock(Client::class);
+    }
+
+    public function getFeeder($client)
+    {
+        return new Feeder($client);
     }
 }
